2d39394f7c5294a95bd50c08a4b025413398d78e
Артём Красов <artkrasov@gmail.com>
2026-06-06 19:16:17 +0300

    Add project files.

---
+++ Lesson1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.InteropServices;
+
+Example1();
+Example2();
+
+static void Example1()
+{
+    var result = MyMessageBox(0, "Заголовок", "Заголовок", 0);
+    Console.WriteLine(result);
+}
+
+static void Example2()
+{
+    var a = int.Parse(Console.ReadLine());
+    var b = int.Parse(Console.ReadLine());
+    Console.WriteLine($"{a} + {b} = {Add(a, b)}");
+}
+
+[DllImport("SimpleMath.dll", EntryPoint = "Add")]
+static extern int Add(int a, int b);
+
+[DllImport("user32.dll", CharSet = CharSet.Unicode, EntryPoint = "MessageBox")] 
+static extern int MyMessageBox(IntPtr hWnd, string text, string caption, uint type);
---
+++ MathLibrary/pch.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include <Windows.h>
+
+extern "C" __declspec(dllexport) int Add(int a, int b)
+{
+	return a + b;
+}
