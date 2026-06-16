43ad1f671a0c62468971ba85589f2ffc2caf202f
Артём Красов <artkrasov@gmail.com>
2026-06-16 17:14:21 +0300

    Добавил третье занятие

---
+++ Lesson2/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using System.Net.Http;
+using System.Windows;
+
+namespace Lesson2;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private Process? _childProcess;
+    public MainWindow()
+    {
+        InitializeComponent();
+
+
+        //Stream = Поток
+        //Thread = Нить
+        //Process -> Набор нитей
+    }
+
+    private void StartProcess_Button_Click(object sender, RoutedEventArgs e)
+    {
+        _childProcess = Process.Start("D:\\projects\\source\\repos\\SystemProgrammingVPU422\\Lesson2.ChildProcess\\bin\\Debug\\net10.0-windows\\Lesson2.ChildProcess.exe");
+    }
+
+    private void KillProcess_Button_Click(object sender, RoutedEventArgs e)
+    {
+        if (_childProcess != null)
+        {
+            _childProcess.Kill();
+            _childProcess = null;
+        }
+    }
+}
---
+++ Lesson3/Program.cs
@@ -0,0 +1,46 @@
+﻿const int countKernel = 20;
+const long totalNumbers = 2_000_000_000L;
+ulong countIterations = totalNumbers / countKernel;
+
+var result = new ulong[countKernel];
+var threads = new Thread[countKernel];
+
+for (int i = 0; i < countKernel; i++)
+{
+    ulong left = 1 + countIterations * (ulong)i;
+    ulong right = countIterations * ((ulong)i + 1);
+
+    int threadIndex = i;
+
+    threads[i] = new Thread(() => CalculateSum(left, right, threadIndex, result));
+}
+
+for (int i = 0; i < threads.Length; i++)
+{
+    threads[i].Start();
+}
+
+for (int i = 0; i < threads.Length; i++)
+{
+    threads[i].Join();
+}
+
+ulong sum = 0;
+for (int i = 0; i < result.Length; i++)
+{
+    sum += result[i];
+}
+
+Console.WriteLine(sum.ToString());
+
+
+static void CalculateSum(ulong left, ulong right, int threadIndex, ulong[] result)
+{
+    ulong sum = 0;
+    for (ulong i = left; i <= right; i++)
+    {
+        sum += i;
+    }
+
+    result[threadIndex] = sum;
+}
