a7f38a137160b0cbb79904c0124073c6201bb4ca
Артём Красов <artkrasov@gmail.com>
2026-06-18 20:06:10 +0300

    Добавил wpf

--- Lesson5.Desktop/MainWindow.xaml.cs
+++ Lesson5.Desktop/MainWindow.xaml.cs
@@ -1,24 +1,63 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
-namespace Lesson5.Desktop
+namespace Lesson5.Desktop;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    private CancellationTokenSource? _cancellationTokenSource;
+    public MainWindow()
     {
-        public MainWindow()
+        InitializeComponent();
+    }
+
+    private void SumButton_Click(object sender, RoutedEventArgs e)
+    {
+        ulong number = ulong.Parse(Number.Text);
+        _cancellationTokenSource = new CancellationTokenSource();
+        var cancellationToken = _cancellationTokenSource.Token;
+
+        ChangeStatusControlElements();
+        ThreadPool.QueueUserWorkItem(_ => CalculateSum(number, cancellationToken));
+    }
+
+    private void CalculateSum(ulong number, CancellationToken cancellationToken)
+    {
+        try
         {
-            InitializeComponent();
+            ulong sum = 0;
+            for (ulong i = 1; i <= number; i++)
+            {
+                sum += i;
+                cancellationToken.ThrowIfCancellationRequested();
+            }
+
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                Result.Text = sum.ToString();
+            });
         }
+        catch (OperationCanceledException)
+        {
+            MessageBox.Show("Операция успешно отменена");
+        }
+        finally
+        {
+            Application.Current.Dispatcher.Invoke(ChangeStatusControlElements);
+        }
+    }
+
+    private void ChangeStatusControlElements()
+    {
+        Number.IsEnabled = !Number.IsEnabled;
+        SumButton.IsEnabled = !SumButton.IsEnabled;
+        CancellationSumButton.IsEnabled = !CancellationSumButton.IsEnabled;
+    }
+
+    private void CancellationSumButton_Click(object sender, RoutedEventArgs e)
+    {
+        _cancellationTokenSource?.Cancel();
     }
 }
