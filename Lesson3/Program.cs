396f439afd783a693aed14decc418cc692b7847b
Артём Красов <artkrasov@gmail.com>
2026-06-18 18:51:55 +0300

    Добавил занятие

--- Lesson3/Program.cs
+++ Lesson3/Program.cs
@@ -1,46 +1,91 @@
-﻿const int countKernel = 20;
-const long totalNumbers = 2_000_000_000L;
-ulong countIterations = totalNumbers / countKernel;
+﻿using System.Diagnostics;
 
-var result = new ulong[countKernel];
-var threads = new Thread[countKernel];
-
-for (int i = 0; i < countKernel; i++)
+public class Program
 {
-    ulong left = 1 + countIterations * (ulong)i;
-    ulong right = countIterations * ((ulong)i + 1);
+    private const int countKernel = 20;
 
-    int threadIndex = i;
-
-    threads[i] = new Thread(() => CalculateSum(left, right, threadIndex, result));
-}
-
-for (int i = 0; i < threads.Length; i++)
-{
-    threads[i].Start();
-}
-
-for (int i = 0; i < threads.Length; i++)
-{
-    threads[i].Join();
-}
-
-ulong sum = 0;
-for (int i = 0; i < result.Length; i++)
-{
-    sum += result[i];
-}
-
-Console.WriteLine(sum.ToString());
-
-
-static void CalculateSum(ulong left, ulong right, int threadIndex, ulong[] result)
-{
-    ulong sum = 0;
-    for (ulong i = left; i <= right; i++)
+    public static void Main()
     {
-        sum += i;
+        ulong div = 2;
+        ulong number = 6_000_000_000L;
+
+        ulong myResult = number * (number + 1) / div;
+        Console.WriteLine(myResult);
+
+        Stopwatch stopwatch = Stopwatch.StartNew();
+
+        ulong mySum = 0;
+        for (ulong i = 1; i <= number; i++)
+        {
+            mySum += i;
+        }
+
+        stopwatch.Stop();
+
+        // Retrieve results using different properties
+        TimeSpan ts = stopwatch.Elapsed;
+        long ms = stopwatch.ElapsedMilliseconds;
+
+        Console.WriteLine($"Total Time: {ts}");
+        Console.WriteLine($"Milliseconds: {ms} ms");
+
+        Console.WriteLine(mySum);
+
+        stopwatch = Stopwatch.StartNew();
+
+        ulong countIterations = number / countKernel;
+
+        var result = new ulong[countKernel];
+        var threads = new Thread[countKernel];
+
+        for (int i = 0; i < countKernel; i++)
+        {
+            ulong left = 1 + countIterations * (ulong)i;
+            ulong right = countIterations * ((ulong)i + 1);
+
+            int threadIndex = i;
+
+            threads[i] = new Thread(() => CalculateSum(left, right, threadIndex, result));
+        }
+
+        for (int i = 0; i < threads.Length; i++)
+        {
+            threads[i].Start();
+        }
+
+        for (int i = 0; i < threads.Length; i++)
+        {
+            threads[i].Join();
+        }
+
+        ulong sum = 0;
+        for (int i = 0; i < result.Length; i++)
+        {
+            sum += result[i];
+        }
+
+        stopwatch.Stop();
+
+        ts = stopwatch.Elapsed;
+        ms = stopwatch.ElapsedMilliseconds;
+
+        Console.WriteLine($"Total Time: {ts}");
+        Console.WriteLine($"Milliseconds: {ms} ms");
+
+        Console.WriteLine(sum.ToString());
     }
 
-    result[threadIndex] = sum;
+    //1..200_000_000
+    //200_000_001..400_000_00
+    //
+    static void CalculateSum(ulong left, ulong right, int threadIndex, ulong[] result)
+    {
+        ulong sum = 0;
+        for (ulong i = left; i <= right; i++)
+        {
+            sum += i;
+        }
+
+        result[threadIndex] = sum;
+    }
 }
---
+++ Lesson4/Program.cs
@@ -0,0 +1,23 @@
+﻿ThreadPool.QueueUserWorkItem(_ => DoWork1());
+ThreadPool.QueueUserWorkItem(_ => DoWork2());
+
+for (int i = 0; i <= 1_000; i++)
+{
+    Console.WriteLine($"Выполняется главная нить: {i}");
+}
+
+void DoWork1()
+{
+    for (int i = 0; i <= 1_000; i++)
+    {
+        Console.WriteLine($"Выполняется дочерняя 1 нить: {i}");
+    }
+}
+
+void DoWork2()
+{
+    for (int i = 0; i <= 1_000; i++)
+    {
+        Console.WriteLine($"Выполняется дочерняя 2 нить: {i}");
+    }
+}
