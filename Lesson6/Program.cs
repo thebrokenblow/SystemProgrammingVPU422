bd5a5c3353e45bca57ae8e37879d42ee5db2353e
Артём Красов <artkrasov@gmail.com>
2026-06-20 18:44:14 +0300

    Занятие 6

---
+++ Lesson6/Program.cs
@@ -0,0 +1,38 @@
+﻿var mutex = new Mutex();
+
+int x = 0;
+
+var threads = new Thread[5];
+
+for (int i = 0; i < threads.Length; i++)
+{
+    var numberThread = i;
+    threads[i] = new Thread(() => Print(numberThread));
+}
+
+for (int i = 0; i < threads.Length; i++)
+{
+    threads[i].Start();
+}
+
+for (int i = 0; i < threads.Length; i++)
+{
+    threads[i].Join();
+}
+
+Console.WriteLine(x);
+
+void Print(int numberThread)
+{
+    mutex.WaitOne();
+
+    x = 0;
+    for (int i = 0; i < 6; i++)
+    {
+        x++;
+        Console.WriteLine($"выполняется нить: {numberThread}");
+        Thread.Sleep(100);
+    }
+
+    mutex.ReleaseMutex();
+}
