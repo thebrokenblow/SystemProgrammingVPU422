0ed480998066ebbe5abd48eee82b1a4a33e85a19
Артём Красов <artkrasov@gmail.com>
2026-06-23 18:26:11 +0300

    7 Занятие

---
+++ Lesson7/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using LoadingSpinnerControl;
+using System.IO;
+using System.Windows;
+
+namespace Lesson7;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private async void Button_Click(object sender, RoutedEventArgs e)
+    {
+        Menu.Visibility = Visibility.Hidden;
+        LoadingSpinner.IsLoading = true;
+
+        var lines = await File.ReadAllLinesAsync("D:\\Users\\artem\\data.txt");
+
+        LoadingSpinner.IsLoading = false;
+        Menu.Visibility = Visibility.Visible;
+    }
+}
